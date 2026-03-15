dd5327818c5eb4d3e15a217ceae1fe44391c1ce7
varun <varunpshrivathsa@gmail.com>
2026-03-15 17:37:37 -0500

    Added Research Images

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -4,35 +4,69 @@ export const metadata = { title: "Research & Publications" };
 
 export default function ResearchPage() {
   return (
-    <Column maxWidth="xl" gap="l" paddingY="16">
-
+    <Column maxWidth="l" gap="l" paddingY="16">
       <Heading variant="display-strong-s">Research & Publications</Heading>
 
-      <Column padding="20" radius="l" border="neutral-alpha-medium" gap="12">
-        <Heading variant="heading-strong-m">
-          Environmental Impact Analysis using Satellite Image Processing:
-          A Case Study on Bangalore STRR Phase-1
-        </Heading>
+      <Row
+        padding="20"
+        radius="l"
+        border="neutral-alpha-medium"
+        gap="24"
+        horizontal="space-between"
+        vertical="start"
+        s={{ direction: "column" }}
+      >
+        {/* Left side */}
+        <Column gap="12" flex={1}>
+          <Heading variant="heading-strong-m">
+            Environmental Impact Analysis using Satellite Image Processing:
+            <br />
+            A Case Study on Bangalore STRR Phase-1
+          </Heading>
 
-        <Text onBackground="neutral-medium">
-          <b>V. P. Shrivathsa</b>, G. Singh BS, S. Reddy U, R. T M
-        </Text>
+          <Text onBackground="neutral-medium">
+            <b>V. P. Shrivathsa</b>, G. Singh BS, S. Reddy U, R. T M
+          </Text>
 
-        <Row gap="12">
-          <Badge>IEEE ASIANCON 2024</Badge>
+          <Row gap="12">
+            <Badge>IEEE ASIANCON 2024</Badge>
+          </Row>
+
+          <Row gap="12">
+            <Button
+              href="https://ieeexplore.ieee.org/document/10838099"
+              prefixIcon="openLink"
+              size="s"
+            >
+              View Paper
+            </Button>
+          </Row>
+        </Column>
+
+        {/* Right side images */}
+        <Row gap="12" s={{ horizontal: "start" }}>
+          <img
+            src="/research/strr-1.png"
+            alt="Research figure 1"
+            style={{
+              width: "120px",
+              height: "80px",
+              objectFit: "cover",
+              borderRadius: "10px",
+            }}
+          />
+          <img
+            src="/research/strr-2.png"
+            alt="Research figure 2"
+            style={{
+              width: "120px",
+              height: "80px",
+              objectFit: "cover",
+              borderRadius: "10px",
+            }}
+          />
         </Row>
-
-        <Row gap="12">
-          <Button
-            href="https://ieeexplore.ieee.org/document/10838099"
-            prefixIcon="openLink"
-            size="s"
-          >
-            View Paper
-          </Button>
-        </Row>
-      </Column>
-
+      </Row>
     </Column>
   );
 }
